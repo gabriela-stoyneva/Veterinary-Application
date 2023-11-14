15fd584e27352c3cf7c5b1cd220c6cc17b31ca1b
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-11-14 14:05:55 +0200

    footer styling

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
 
     return (
         <footer>
-            <p>All rights reserved! &copy;</p>
+            <p> &copy; All rights reserved!</p>
+            <p><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-twitter"></i><i class="fa-regular fa-envelope-open"></i></p>
         </footer>
     )
 }
