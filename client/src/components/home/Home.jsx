a19feaac46d61167292a0428054dec29b5723301
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-11-14 15:59:24 +0200

    home structure and style

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,14 +1,21 @@
 import style from './Home.module.css'
+import kitten from '../../assets/pictures/kitten.jpg'
 
 
 export default function Home() {
 
     return (
-        <div className="home">
+        <div className={style.home}>
+            <div className={style.media}><img src={kitten} alt="" /></div>
+            <div className={style.heading}>
+                <h1>
+                    Welcome to Happy Pets Clinic!
+                </h1>
+                <button>
+                    make an appointment
+                </button>
+            </div>
 
-            <h1>
-                Hello
-            </h1>
         </div>
 
 
