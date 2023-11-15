d121c114b20336a5e221d7d687dd363d64c9bff6
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-11-15 11:41:52 +0200

    style changes

---
+++ client/src/components/home/AboutUs.jsx
@@ -0,0 +1,18 @@
+import style from './AboutUs.module.css';
+
+export default function AboutUs() {
+
+    return (
+        <section className={style.aboutUs}>
+            <div className={style.info}>
+                <p>
+                    
+                </p>
+            </div>
+            <div className={style.media}>
+                <img src="" alt="" />
+            </div>
+
+        </section>
+    )
+}
