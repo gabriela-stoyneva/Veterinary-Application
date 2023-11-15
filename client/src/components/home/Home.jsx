4ed93531faa45a702e82443062cc39bc6b80029d
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-11-15 09:39:02 +0200

    sevices home page component structure and style

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,22 +1,27 @@
 import style from './Home.module.css'
 import kitten from '../../assets/pictures/kitten.jpg'
+import Services from './Services'
 
 
 export default function Home() {
 
     return (
-        <div className={style.home}>
-            <div className={style.media}><img src={kitten} alt="" /></div>
-            <div className={style.heading}>
-                <h1>
-                    Welcome to Happy Pets Clinic!
-                </h1>
-                <button>
-                    make an appointment
-                </button>
-            </div>
+        <>
+            <div className={style.home}>
+                <div className={style.media}><img src={kitten} alt="" /></div>
+                <div className={style.heading}>
+                    <h1>
+                        Welcome to <span className={style.name}>Happy Pets</span> Clinic!
+                    </h1>
+                    <button>
+                        make an appointment
+                    </button>
+                </div>
 
-        </div>
+            </div>
+            <Services />
+        </>
+
 
 
     )
---
+++ client/src/components/home/Services.jsx
@@ -0,0 +1,41 @@
+import style from './Services.module.css'
+
+export default function Services() {
+
+    return (
+        <section className={style.services}>
+            <article className={style.articleService}>
+                <p><i class="fa-solid fa-shield-dog"></i></p>
+                <h3>Preventative Care/Wellness</h3>
+                <p>At La Verne Animal Hospital, our approach to veterinary care is to promote excellent overall…</p>
+            </article>
+            <article className={style.articleService}>
+                <p><i class="fa-solid fa-house-medical"></i></p>
+                <h3>Emergency Medicine & Urgent Care</h3>
+                <p>Health problems, injuries, and illnesses can happen at any time, and may need immediate attention.…</p>
+                
+            </article>
+            <article className={style.articleService}>
+                <p><i class="fa-solid fa-prescription"></i></p>
+                <h3>In-House Diagnostics</h3>
+                <p>When your pet is not feeling well, one of the first things a veterinarian will…</p>
+            </article>
+            <article className={style.articleService}>
+                <p><i class="fa-solid fa-tooth"></i></p>
+                <h3>Dentistry</h3>
+                <p>Do you know the number one disease affecting cats and dogs today? If you guessed…</p>
+            </article>
+            <article className={style.articleService}>
+                <p><i class="fa-solid fa-microchip"></i></p>
+                <h3>Microchipping</h3>
+                <p>We encourage all pet owners to microchip their animal companions. The sad fact is that…</p>
+            </article>
+            <article className={style.articleService}>
+                <p><i class="fa-solid fa-user-nurse"></i></p>
+                <h3>Surgery</h3>
+                <p>Our hospital provides a full range of surgical services, from standard spaying and neutering to…</p>
+            </article>
+
+        </section>
+    )
+}
