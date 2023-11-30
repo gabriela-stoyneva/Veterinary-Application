3b80d83e0135679f4cc2c936f99e5cc347e45004
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-11-30 13:31:18 +0200

    update navigation - contact us structure

--- client/src/components/header/Navigation.jsx
+++ client/src/components/header/Navigation.jsx
@@ -12,56 +12,70 @@ export default function Navigation() {
             <ul>
                 <li className={style.stateless}> <Link to="/">Home</Link></li>
                 <li className={style.stateless}> <Link to="/services"></Link> Services</li>
-                <li className={style.stateless}> <Link to="/contact-us">Contact us</Link></li>
                 <li>
-                <span>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="secondary" id="dropdown-basic" className={style.dropdown}>
-                        Adopt
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="/find">FIND PET</Dropdown.Item>
-                            <Dropdown.Item href="/add">ADD ANIMAL IN NEED</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </span>
-                </li> 
-                
-                <li>
-                <span>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="secondary" id="dropdown-basic" className={style.dropdown}>
-                            About Us
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="/hospital" >OUR HOSPITAL</Dropdown.Item>
-                            <Dropdown.Item href="/doctors">OUR DOCTORS</Dropdown.Item>
-                            <Dropdown.Item href="/team">OUR TEAM</Dropdown.Item>
-                            <Dropdown.Item href="/news">WHAT'S NEW</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </span>
+                    <span>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic" className={style.dropdown}>
+                                About Us
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="/hospital" >OUR HOSPITAL</Dropdown.Item>
+                                <Dropdown.Item href="/doctors">OUR DOCTORS</Dropdown.Item>
+                                <Dropdown.Item href="/team">OUR TEAM</Dropdown.Item>
+                                <Dropdown.Item href="/news">WHAT'S NEW</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </span>
                 </li>
                 <li>
-                <span>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="secondary" id="dropdown-basic" className={style.dropdown}>
-                            Clients
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="/login" >LOGIN</Dropdown.Item>
-                            <Dropdown.Item href="/register">REGISTER</Dropdown.Item>
-                            <Dropdown.Item href="/logout">LOGOUT</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </span>
+                    <span>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic" className={style.dropdown}>
+                                CONTACT US
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="/contact-us">CONTACT US</Dropdown.Item>
+                                <Dropdown.Item href="/questions">COMMONLY ASKED QUESTIONS</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </span>
 
                 </li>
+                <li>
+                    <span>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic" className={style.dropdown}>
+                                Adopt
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="/find">FIND PET</Dropdown.Item>
+                                <Dropdown.Item href="/add">ADD ANIMAL IN NEED</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </span>
+                </li>
+
                
-               
-                
+                <li>
+                    <span>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic" className={style.dropdown}>
+                                Clients
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="/login" >LOGIN</Dropdown.Item>
+                                <Dropdown.Item href="/register">REGISTER</Dropdown.Item>
+                                <Dropdown.Item href="/logout">LOGOUT</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </span>
+
+                </li>
+
+
+
             </ul>
-            
+
 
 
         </nav>
