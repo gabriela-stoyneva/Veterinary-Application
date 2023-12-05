237cef3fd3b1233a1d03455ff9df88211bf43c11
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-12-05 14:11:11 +0200

    dynamic doctors data

--- client/src/components/about/Doctors.jsx
+++ client/src/components/about/Doctors.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react"
+
 export default function Doctors() {
+
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/doctors')
+            .then((res)=> res.json())
+            .then((data)=> setDoctors(Object.values(data)))
+            .catch((error)=>console.log(error))
+
+    }, [])
 
     return (
         <div className="doctors">
             <h3>OUR DOCTORS</h3>
-            <article>
-                <div className="media">
-                    <img src="" alt="" />
+            {doctors.map((doctor) => <article key={doctor._id}>
+                <div >
+                    <img src={doctor.imageUrl} alt={"doctor's picture"} />
                 </div>
-                <p></p>
-            </article>
+                <p>{doctor.name}</p>
+            </article>)}
+            
         </div>
     )
 }
