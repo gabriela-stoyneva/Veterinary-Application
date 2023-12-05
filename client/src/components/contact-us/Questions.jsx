e689b5b6691c80099f4ba78cfb7eca101687d5d5
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-12-05 19:47:53 +0200

    style question area

--- client/src/components/contact-us/Questions.jsx
+++ client/src/components/contact-us/Questions.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import style from './Questions.module.css';
+
 export default function Questions() {
 
     const [questions, setQuestions] = useState([]);
@@ -14,16 +16,19 @@ export default function Questions() {
 
     return (
         <>
-            <div className="questions">
+            <div className={style.questions}>
 
                 <h3>Common medical concerns and frequently asked questions.</h3>
+                
+                <div className={style.questionArea}>
+                    {questions.map((question) => (
+                        <div className={style.currentQuestion}>
+                            <h5>{question.name}</h5>
+                            <p>{question.answer}</p>
+                        </div>
+                    ))}
+                </div>
 
-                {questions.map((question) => (
-                    <div className="currentQuestion">
-                        <h5>{question.name}</h5>
-                        <p>{question.answer}</p>
-                    </div>
-                ))}
 
             </div>
         </>
