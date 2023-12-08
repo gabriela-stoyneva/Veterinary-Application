f9e97c4e9efda883a61815f208e4a90fbbce4b5c
gabriela-stoineva-91 <gabito01042011@gmail.com>
2023-12-08 21:39:03 +0200

    register function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,34 +18,37 @@ import Questions from './components/contact-us/Questions';
 import Appointment from './components/contact-us/Appointment';
 import AddAnimalForm from './components/adoption/AddAnimalForm';
 import PetList from './components/adoption/PetList';
+import { AuthProvider } from './contexts/authContext';
 
 
 function App() {
 
   return (
-    <div className='root'>
-      <Header />
+    <AuthProvider>
+      <div className='root'>
+        <Header />
 
-      <main>
-        <Routes>
-          <Route path={Path.Home} element={<Home />} />
-          <Route path={Path.Login} element={<LoginForm />} />
-          <Route path={Path.Register} element={<RegisterForm />} />
-          <Route path={Path.ContactUs} element={<ContactUsForm />} />
-          <Route path={Path.Hospital} element={<Hospital />} />
-          <Route path={Path.Doctors} element={<Doctors /> } />
-          <Route path={Path.Team} element={<Team /> } />
-          <Route path={Path.Services} element={<Services />} />
-          <Route path={Path.Questions} element={<Questions />}   />
-          <Route path={Path.Appointment} element={<Appointment />} />  
-          <Route path={Path.Find} element={<PetList /> }  />     
-          <Route path={Path.Add} element={<AddAnimalForm />} />
+        <main>
+          <Routes>
+            <Route path={Path.Home} element={<Home />} />
+            <Route path={Path.Login} element={<LoginForm />} />
+            <Route path={Path.Register} element={<RegisterForm />} />
+            <Route path={Path.ContactUs} element={<ContactUsForm />} />
+            <Route path={Path.Hospital} element={<Hospital />} />
+            <Route path={Path.Doctors} element={<Doctors />} />
+            <Route path={Path.Team} element={<Team />} />
+            <Route path={Path.Services} element={<Services />} />
+            <Route path={Path.Questions} element={<Questions />} />
+            <Route path={Path.Appointment} element={<Appointment />} />
+            <Route path={Path.Find} element={<PetList />} />
+            <Route path={Path.Add} element={<AddAnimalForm />} />
 
-        </Routes>
-      </main>
-     
-      <Footer />
-    </div>
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </AuthProvider>
   )
 }
 
--- client/src/components/clients/RegisterForm.jsx
+++ client/src/components/clients/RegisterForm.jsx
@@ -2,22 +2,20 @@ import { useContext } from "react";
 
 import style from './Form.module.css';
 
-import AuthContext from "../../contexts/authContext";
+import AuthContext from '../../contexts/authContext';
 import useForm from "../../hooks/useForm";
 
 
 import { Link } from "react-router-dom";
 import Path from '../../lib/paths';
 
-
+const RegisterFormKeys = {
+    Email: 'email',
+    Password: 'password',
+    RepeatPassword: 'confirm-password',
+};
 
 export default function RegisterForm() {
-
-    const RegisterFormKeys = {
-        Email: 'email',
-        Password: 'password',
-        RepeatPassword: 'confirm-password',
-    };
 
     const { registerSubmitHandler } = useContext(AuthContext);
 
@@ -27,9 +25,8 @@ export default function RegisterForm() {
         [RegisterFormKeys.ConfirmPassword]: '',
     });
 
-
     return (
-        <form method="POST" className={style.register} onSubmit={onSubmit}>
+        <form className={style.register} onSubmit={onSubmit}>
 
             <h2>Register</h2>
 
@@ -70,8 +67,8 @@ export default function RegisterForm() {
             </div>
 
             <div>
-                <button type="submit">Register</button>
-                <p>Do you have account? Login <Link to={Path.Login}> here! </Link> </p>
+                <button type="submit" value="Register">Register</button>
+                <p className={style.message}>Do you have account? Login <Link to={Path.Login}><span className={style.here}>here!</span></Link> </p>
             </div>
 
         </form>
