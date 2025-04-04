cd538df85b4b63fd2d91c8bd5ff6e84df844d98a
gabriela-stoineva-91 <gabito01042011@gmail.com>
2025-04-04 18:01:52 +0300

    Update Team.jsx

--- client/src/components/about/Team.jsx
+++ client/src/components/about/Team.jsx
@@ -7,7 +7,7 @@ export default function Team() {
         <div className={style.team}>
             <h3>OUR TEAM</h3>
             <div className={style.container}>
-                <p>Our friendly and supportive staff genuinely cares for your four-legged companions. Dr. Niu has recently joined us as practice owner, and our dedicated staff remains the same. You will often see the same smiling faces each time you visit! Our comprehensive approach to pet health involves a team of talented and caring animal lovers devoted to your pet’s specific health needs.</p>
+                <p>Our friendly, careful and supportive staff genuinely cares for your four-legged companions. Dr. Niu has recently joined us as practice owner, and our dedicated staff remains the same. You will often see the same smiling faces each time you visit! Our comprehensive approach to pet health involves a team of talented and caring animal lovers devoted to your pet’s specific health needs.</p>
                 <div className={style.media}>
                     <img src={doctors} alt="team" />
                 </div>
